39ae4115ebeadade49e681c3a713243434de2eb1
Siva Bhupani <siva.bhupani@peopletech.com>
2025-01-09 14:47:44 +0530

    service-registry-and-discover-done

---
+++ movie-booking-api/theaterservice/src/main/java/com/moviebooking/theaterservice/TheaterserviceApplication.java
@@ -0,0 +1,13 @@
+package com.moviebooking.theaterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheaterserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheaterserviceApplication.class, args);
+	}
+
+}
---
+++ movie-booking-api/theaterservice/src/test/java/com/moviebooking/theaterservice/TheaterserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviebooking.theaterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheaterserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
